ca6680c8efe19bc11847d86ceea58863d165e620
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-21 17:38:29 -0400

    minute to second

---
+++ src/com/haley/jsFoundations/mintosec.java
@@ -0,0 +1,9 @@
+package com.haley.jsFoundations;
+
+public class mintosec {
+
+    public static void main(String[] args){
+    int minuteInput = 60;
+            int secondOutput = minuteInput *60;
+                    System.out.println(secondOutput);
+}}
